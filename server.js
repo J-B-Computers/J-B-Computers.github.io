7ecd4cf325a22b8604b95f728a830f9a1d370857
Benson Basil <jbcom.general@gmail.com>
2024-06-18 02:46:38 +1000

    UPDATE CODE

--- server.js
+++ server.js
@@ -2,21 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = 3001; // Change this line to use a different port
+const port = 3001;
 
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve static files
+app.use('/styles', express.static(path.join(__dirname, 'styles')));
+app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'templates'));
 
 // Routes
-app.use('/', require('./routes/index'));
+app.get('/', (req, res) => {
+    const content = require('./data/content.json');
+    res.render('index', { content });
+});
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
