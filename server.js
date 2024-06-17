e8ae4f09be401eb788859258ef06b8ad1e2856fd
Benson Basil <jbcom.general@gmail.com>
2024-06-18 01:38:33 +1000

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // server.js
-const express = require('express');
+const express = require('express'); // Ensure this is declared only once
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
@@ -16,31 +16,6 @@ app.set('views', path.join(__dirname, 'templates'));
 
 // Routes
 app.use('/', require('./routes/index'));
-
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const app = express();
-const port = 3000;
-
-// Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Set the view engine to EJS
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'templates'));
-
-// Routes
-app.get('/', (req, res) => {
-    const content = require('./data/content.json');
-    res.render('index', { content: content });
-});
 
 // Start the server
 app.listen(port, () => {
