112a672eb7dd8b3d1457006a23df5a84b647e109
Benson Basil <jbcom.general@gmail.com>
2024-06-18 03:31:35 +1000

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = 3001;
+const port = 3001; // Ensure no conflicts with this port
 
 // Middleware
 app.use(bodyParser.json());
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Serve static files
 app.use('/styles', express.static(path.join(__dirname, 'styles')));
 app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
@@ -19,8 +19,9 @@ app.set('views', path.join(__dirname, 'templates'));
 
 // Routes
 app.get('/', (req, res) => {
-    const content = require('./data/content.json');
-    res.render('index', { content });
+    res.render('index', {
+        content: require('./assets/data/content.json')
+    });
 });
 
 app.listen(port, () => {
