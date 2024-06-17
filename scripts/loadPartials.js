3e15318c8ccd63b0ce9d5678cd7d1bb49be9b22a
Benson Basil <jbcom.general@gmail.com>
2024-06-18 02:47:42 +1000

    Update loadPartials.js

--- scripts/loadPartials.js
+++ scripts/loadPartials.js
@@ -1,23 +1,4 @@
-// Function to load an HTML file and insert its content into the specified element
-function loadHTML(file, elementId) {
-    fetch(file)
-        .then(response => {
-            if (response.ok) {
-                return response.text();
-            }
-            throw new Error('Network response was not ok.');
-        })
-        .then(data => {
-            document.getElementById(elementId).innerHTML = data;
-        })
-        .catch(error => {
-            console.error('Error fetching and parsing data:', error);
-        });
-}
-
-// Load header, footer, and sidebar
-document.addEventListener('DOMContentLoaded', function() {
-    loadHTML('templates/header.html', 'header-placeholder');
-    loadHTML('templates/footer.html', 'footer-placeholder');
-    loadHTML('templates/sidebar.html', 'sidebar-placeholder');
+$(document).ready(function() {
+    $("#header-placeholder").load("/templates/header.ejs");
+    $("#footer-placeholder").load("/templates/footer.ejs");
 });
